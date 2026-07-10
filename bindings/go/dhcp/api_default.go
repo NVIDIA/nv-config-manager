22658150134c21d3a13a23ed0208b8243e232bf3
rheffernan <rheffernan@nvidia.com>
2026-07-10 09:17:54 -0700

    chore(api): generate DHCP lease artifacts
    Signed-off-by: rheffernan <rheffernan@nvidia.com>

--- bindings/go/dhcp/api_default.go
+++ bindings/go/dhcp/api_default.go
@@ -367,6 +367,127 @@ func (a *DefaultAPIService) HealthcheckHealthcheckGetExecute(r ApiHealthcheckHea
 	return localVarReturnValue, localVarHTTPResponse, nil
 }
 
+type ApiProxyLeaseCommandLeasePostRequest struct {
+	ctx          context.Context
+	ApiService   *DefaultAPIService
+	leaseRequest *LeaseRequest
+}
+
+func (r ApiProxyLeaseCommandLeasePostRequest) LeaseRequest(leaseRequest LeaseRequest) ApiProxyLeaseCommandLeasePostRequest {
+	r.leaseRequest = &leaseRequest
+	return r
+}
+
+func (r ApiProxyLeaseCommandLeasePostRequest) Execute() (interface{}, *http.Response, error) {
+	return r.ApiService.ProxyLeaseCommandLeasePostExecute(r)
+}
+
+/*
+ProxyLeaseCommandLeasePost Proxy Lease Command
+
+Proxy a supported IPv4 lease command to KEA.
+
+	@param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+	@return ApiProxyLeaseCommandLeasePostRequest
+*/
+func (a *DefaultAPIService) ProxyLeaseCommandLeasePost(ctx context.Context) ApiProxyLeaseCommandLeasePostRequest {
+	return ApiProxyLeaseCommandLeasePostRequest{
+		ApiService: a,
+		ctx:        ctx,
+	}
+}
+
+// Execute executes the request
+//
+//	@return interface{}
+func (a *DefaultAPIService) ProxyLeaseCommandLeasePostExecute(r ApiProxyLeaseCommandLeasePostRequest) (interface{}, *http.Response, error) {
+	var (
+		localVarHTTPMethod  = http.MethodPost
+		localVarPostBody    interface{}
+		formFiles           []formFile
+		localVarReturnValue interface{}
+	)
+
+	localBasePath, err := a.client.cfg.ServerURLWithContext(r.ctx, "DefaultAPIService.ProxyLeaseCommandLeasePost")
+	if err != nil {
+		return localVarReturnValue, nil, &GenericOpenAPIError{error: err.Error()}
+	}
+
+	localVarPath := localBasePath + "/lease"
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := url.Values{}
+	localVarFormParams := url.Values{}
+	if r.leaseRequest == nil {
+		return localVarReturnValue, nil, reportError("leaseRequest is required and must be specified")
+	}
+
+	// to determine the Content-Type header
+	localVarHTTPContentTypes := []string{"application/json"}
+
+	// set Content-Type header
+	localVarHTTPContentType := selectHeaderContentType(localVarHTTPContentTypes)
+	if localVarHTTPContentType != "" {
+		localVarHeaderParams["Content-Type"] = localVarHTTPContentType
+	}
+
+	// to determine the Accept header
+	localVarHTTPHeaderAccepts := []string{"application/json"}
+
+	// set Accept header
+	localVarHTTPHeaderAccept := selectHeaderAccept(localVarHTTPHeaderAccepts)
+	if localVarHTTPHeaderAccept != "" {
+		localVarHeaderParams["Accept"] = localVarHTTPHeaderAccept
+	}
+	// body params
+	localVarPostBody = r.leaseRequest
+	req, err := a.client.prepareRequest(r.ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, formFiles)
+	if err != nil {
+		return localVarReturnValue, nil, err
+	}
+
+	localVarHTTPResponse, err := a.client.callAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	localVarBody, err := io.ReadAll(localVarHTTPResponse.Body)
+	localVarHTTPResponse.Body.Close()
+	localVarHTTPResponse.Body = io.NopCloser(bytes.NewBuffer(localVarBody))
+	if err != nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: localVarHTTPResponse.Status,
+		}
+		if localVarHTTPResponse.StatusCode == 422 {
+			var v HTTPValidationError
+			err = a.client.decode(&v, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHTTPResponse, newErr
+			}
+			newErr.error = formatErrorMessage(localVarHTTPResponse.Status, &v)
+			newErr.model = v
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	err = a.client.decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+	if err != nil {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: err.Error(),
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	return localVarReturnValue, localVarHTTPResponse, nil
+}
+
 type ApiWhoamiWhoamiGetRequest struct {
 	ctx        context.Context
 	ApiService *DefaultAPIService
